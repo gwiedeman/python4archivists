c9ae60d16203a661ac32d4ad58a8ba402e8c76e2
Gregory Wiedeman <gwiedeman@albany.edu>
2016-09-19 17:21:47 -0400

    some basic sample scripts

---
+++ equals.py
@@ -0,0 +1,18 @@
+something = "nothing"
+print(something)
+
+#adding together strings
+anotherThing = something + " more things"
+print(anotherThing)
+
+
+print(something) #something is still the same
+
+"""
+This stuff
+won't count
+"""
+
+#do math to integers
+something = 5
+print(something + 15)
