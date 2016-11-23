5ae133155655533432ec1b5ab6d9b89b69a82b9b
Gregory Wiedeman <gwiedeman@albany.edu>
2016-11-23 14:18:45 -0500

    made sample clearer

--- for.py
+++ for.py
@@ -1,12 +1,12 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+manyThings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print("Show each number in the list:")
-for number in list:
+for number in manyThings:
 	print(number)
 	
 print("Show each number in the list times 6:")
 
-for number in list:
+for number in manyThings:
 	print(number * 6)
 	
 # try and add some things to the myList below and write a basic for loop:
@@ -15,7 +15,7 @@ myList = []
 
 for item in myList:
 	pass
-	#do something to item here
-	
+	#remove pass and do something to item here
+	#maybe try myList.append(10)	
 	
 listOfLists = [[01, 02, 03], ["January", "February", "March"]]
